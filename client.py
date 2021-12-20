9476f3d08ebc7f108bb4804bd5588d1cab27f6d7
Andrey Danilov <hangryboy24@gmail.com>
2021-12-20 13:23:29 +0300

    server test

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 import socket
 
-sock = socket.socket()
-sock.connect(('localhost', 5555))
-text = input(">>> ")
-sock.send(text)
+sock = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+sock.connect(('127.0.0.1', 1235))
+while True:
+    text = input(">>> ")
+    sock.send(text)
+    data = sock.recv(1024)
+    print(data.decode('utf-8'))
 
-data = sock.recv(1024)
 sock.close()
 
-print(data)
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 5555))
+sock = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+
+sock.bind(('127.0.0.1', 1235))
 sock.listen(1)
 conn, addr = sock.accept()
 
@@ -11,6 +15,6 @@ while True:
     data = conn.recv(1024)
     if not data:
         break
-    conn.send(bytes(f"[INFO] Data = {data}"))
+    conn.send(f"[INFO] Data = {data}".encode('utf-8'))
 
 conn.close()
